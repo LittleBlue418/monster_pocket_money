4eb8ce2ecc7229c55d6955cb4f86c3098ba1674f
LittleBlue418 <hollyathomas88@gmail.com>
2020-11-18 12:04:52 +0100

    Jobs board listing jobs drom db

--- frontend/src/components/JobsBoard/JobListing/JobListing.js
+++ frontend/src/components/JobsBoard/JobListing/JobListing.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import classes from './JobListing.module.css'
+import { Link } from "react-router-dom"
 
+const JobListing = ({job}) => {
+  const postitClasses = [classes.JobPostit, classes[job.postit_id]]
 
-const JobListing = () => {
+  /* TODO: fix formatting around postit notes */
+
   return (
-    <div>
-      JobListing
-    </div>
+    <Link className={classes.PostitLink} to={"/" + job._id}>
+      <div className={postitClasses.join(' ')}>
+        <div className={classes.PostitBody}>
+          <h5>{job.name}</h5>
+          <p>£{job.reward}</p>
+        </div>
+      </div>
+    </Link>
   )
 }
 
