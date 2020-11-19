329891903b6227da0130861621f6cdfee79962f4
LittleBlue418 <hollyathomas88@gmail.com>
2020-11-19 15:53:23 +0100

    Capitalize job titles

--- frontend/src/components/JobsBoard/JobListing/JobListing.js
+++ frontend/src/components/JobsBoard/JobListing/JobListing.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import classes from './JobListing.module.css'
 import { Link } from "react-router-dom"
+import capitalize from '../../../context/helperFunction'
 
 const JobListing = ({job}) => {
   const postitClasses = [classes[job.postit_id], classes.PostitLink]
 
   /* TODO: logic to display £ if 1 or more, or p if less */
+  /* TODO: capitalize */
 
   return (
     <Link className={postitClasses.join(' ')} to={"/" + job._id}>
-      <h5>{job.name}</h5>
+      <h5>{capitalize(job.name)}</h5>
       <p>£{job.reward}</p>
     </Link>
   )
