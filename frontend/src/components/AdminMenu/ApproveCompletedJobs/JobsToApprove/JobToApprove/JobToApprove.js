64fc6850eb06a1b4c2ab659a3a9b713fd44ec5b3
LittleBlue418 <hollyathomas88@gmail.com>
2020-12-02 15:51:08 +0100

    render jobinstances to be approved

--- frontend/src/components/AdminMenu/ApproveCompletedJobs/JobsToApprove/JobToApprove/JobToApprove.js
+++ frontend/src/components/AdminMenu/ApproveCompletedJobs/JobsToApprove/JobToApprove/JobToApprove.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import classes from './JobToApprove.module.css'
+import { capitalize } from '../../../../../context/helperFunction'
+import '../../../../../context/siteStyling.css'
 
-const JobToApprove = () => {
+const JobToApprove = ( {jobToApprove} ) => {
+  const cssClasses = {
+    jobToApproveContainer: ["siteBoxOutline", classes.JobToApproveContainer],
+    editButton: ["siteButton", ],
+    approvebutton: ["siteButton", "purpleBackground", "whiteText"]
+  }
+
+  const editjobToApprove = () => (
+    console.log("edit button clicked")
+  )
+
+  const approvejobToApprove = () => (
+    console.log("approve button clicked")
+  )
+
   return (
-    <div>A job to approve</div>
+    <div className={cssClasses.jobToApproveContainer.join(' ')}>
+      <div>
+      {capitalize(jobToApprove.job.name)}
+      <span> &nbsp;</span>
+
+      {
+        jobToApprove.participants.map((participant) => (
+          <span key={participant._id}> - {participant.name}</span>
+        ))
+      }
+      </div>
+      <div>
+
+        <button
+          className={cssClasses.editButton.join(' ')}
+          onClick={editjobToApprove}
+        >Edit</button>
+
+        <button
+          className={cssClasses.approvebutton.join(' ')}
+          onClick={approvejobToApprove}
+        >Approve</button>
+
+      </div>
+
+    </div>
   )
 }
 
--- frontend/src/components/AdminMenu/ApproveCompletedJobs/JobsToApprove/JobsToApprove.js
+++ frontend/src/components/AdminMenu/ApproveCompletedJobs/JobsToApprove/JobsToApprove.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useState, useContext, useEffect  } from 'react'
 import JobToApprove from './JobToApprove/JobToApprove'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { APIContext } from '../../../../context/APIContext'
+import classes from './JobsToApprove.module.css'
 
 const JobsToApprove = () => {
+  const API = useContext(APIContext)
+  const [jobsToApprove, setjobsToApprove] = useState(null)
+
+  useEffect(() => {
+    API.list_jobinstances().then((jobInstances) => {
+      setjobsToApprove(jobInstances)
+    })
+  }, [setjobsToApprove, API])
+
+  if (jobsToApprove === null) return <CircularProgress />
+
+  const pageContent = () => {
+    if (jobsToApprove.length === 0) {
+      return (
+        <div className={classes.EmptyNotification}>Hey, it looks like there are no jobs to approve right now!</div>
+      )
+    } else {
+      return(
+        jobsToApprove.map((jobToApprove) => (
+          <JobToApprove
+            key={jobToApprove._id}
+            jobToApprove={jobToApprove}
+          />
+        ))
+      )
+    }
+  }
+
   return (
-    <>
-    <div>Jobs to approve</div>
-    <br></br>
-    <JobToApprove />
-    <JobToApprove />
-    </>
+    pageContent()
   )
 }
 
